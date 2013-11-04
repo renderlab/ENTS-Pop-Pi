df569e88f777e1839d9fba970db9e8e509767733
cgoss <colin.goss@gmail.com>
2013-11-03 18:33:22 -0700

    Removed unused tag info.
    This test only needs to output the contents of what was read from the
    reader.

--- pop-PI/RFIDTest.py
+++ pop-PI/RFIDTest.py
@@ -2,16 +2,7 @@ import serial
 ser = serial.Serial('/dev/ttyAMA0', 2400, timeout=0.5)
 while True:
     string = ser.read(12)
-    if len(string) == 0:
-        print "Please insert a tag"
+    if len(string) > 0:
+        print string
         continue
-    else:
-        string = string[1:11] #exclude start x0A and stop x0D bytes
-        if string == '0415DB18A3':
-            print "You used your black tag"
-        elif string == '0F03028F57':
-            print "You used your white tag"
-        else:
-            print "You do not have a valid tag"
-
-
+    

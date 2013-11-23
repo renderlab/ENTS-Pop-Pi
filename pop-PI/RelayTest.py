8051b075be507c2a87efda11033949899feb197c
Colin Goss <colin.goss@gmail.com>
2013-11-23 15:20:29 -0700

    Updated RelayTest to control RFID reader enable pin
    Adjusted pin's to work with Prototype board.
    Relay set to GPIO 23
    Added test to enable RFID reader by pulling pin to ground.

--- pop-PI/RelayTest.py
+++ pop-PI/RelayTest.py
@@ -1,14 +1,24 @@
 from time import sleep
 import RPi.GPIO as GPIO
-
 GPIO.setmode(GPIO.BCM)
-#enable the gpio
-GPIO.setup(18, GPIO.OUT)
-#set the initial state
-GPIO.output(18, False)
+#old pin 18
+GPIO.setup(23, GPIO.OUT)
+GPIO.output(23, False)
 sleep(1)
-#turn the relay on
-GPIO.output(18, True)
+GPIO.output(23, True)
 sleep(1)
-#turn the relay off.
-GPIO.output(18, False)
+GPIO.output(23, False)
+GPIO.cleanup()
+
+
+
+GPIO.setup(18,GPIO.OUT)
+#GPIO.output(18,False)
+sleep(1)
+GPIO.output(18,True)
+sleep(1)
+GPIO.output(18,False)
+sleep(1)
+GPIO.cleanup()
+
+

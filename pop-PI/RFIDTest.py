6b6f134b5f79a7e20c3fd5c291ec2eefb4c2b22f
Colin Goss <colin.goss@gmail.com>
2013-11-23 15:21:42 -0700

    Added pin to Enable Disable RFID reader
    Added code to make use of the Pin on the Prototyped board to enable or
    disable RFID reader.

--- pop-PI/RFIDTest.py
+++ pop-PI/RFIDTest.py
@@ -1,8 +1,20 @@
+from time import sleep
 import serial
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.OUT)
+
 ser = serial.Serial('/dev/ttyAMA0', 2400, timeout=0.5)
+GPIO.output(18,True)
 while True:
-    string = ser.read(12)
-    if len(string) > 0:
-        print string
-        continue
-    
+        GPIO.output(18,False)
+        string = ser.read(12)
+        GPIO.output(18,True)
+        if len(string) != 0:
+          ser.close()
+          print string
+          sleep(5)
+          ser.open()
+
+GPIO.cleanup()
+

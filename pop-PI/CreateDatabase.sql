5e4e2329bd566018ffa526eb6f2fa57dd2eecf1a
cgoss <colin.goss@gmail.com>
2013-12-02 04:19:00 -0700

    Sql Updates
    Updated initial state of tables.

--- pop-PI/CreateDatabase.sql
+++ pop-PI/CreateDatabase.sql
@@ -9,7 +9,9 @@ use pop_PI;
 
 CREATE TABLE MemberAccount (Id int PRIMARY KEY AUTO_INCREMENT,RFID VARCHAR(20), Account INT, Email VARCHAR(250));
 
-CREATE TABLE PopVend (Id int PRIMARY KEY AUTO_INCREMENT,Pop int));
+CREATE TABLE PopVend (Id int PRIMARY KEY AUTO_INCREMENT,Pop int);
+--setup initial records
+insert PopVend (Pop) values (0);
 
 CREATE TABLE Inventory (Id int PRIMARY KEY AUTO_INCREMENT,Pop int,PopType VARCHAR(20));
-
+insert Inventory (Pop) values (0);
